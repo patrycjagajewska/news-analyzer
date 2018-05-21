3379b9379c25cff4e44c39bc3ecea80d8bb745a6
Piotr Roksela <piotr@roksela.pl>
2018-05-21 09:43:48 +0200

    fixes for master

--- src/main/java/pl/edu/agh/ztis/newsanalyzer/extractors/InteriaExtractor.java
+++ src/main/java/pl/edu/agh/ztis/newsanalyzer/extractors/InteriaExtractor.java
@@ -5,10 +5,12 @@ import org.jsoup.select.Elements;
 import pl.edu.agh.ztis.newsanalyzer.Extractor;
 import pl.edu.agh.ztis.newsanalyzer.Source;
 
+import java.util.Map;
+
 public class InteriaExtractor extends Extractor {
 
-    public InteriaExtractor(String feedUrl, String channel) {
-        super(feedUrl, channel);
+    public InteriaExtractor(String feedUrl, String channel, Map<String, String> dict) {
+        super(feedUrl, channel, dict);
     }
 
     @Override
--- src/main/java/pl/edu/agh/ztis/newsanalyzer/extractors/OnetExtractor.java
+++ src/main/java/pl/edu/agh/ztis/newsanalyzer/extractors/OnetExtractor.java
@@ -5,10 +5,12 @@ import org.jsoup.select.Elements;
 import pl.edu.agh.ztis.newsanalyzer.Extractor;
 import pl.edu.agh.ztis.newsanalyzer.Source;
 
+import java.util.Map;
+
 public class OnetExtractor extends Extractor {
 
-    public OnetExtractor(String feedUrl, String channel) {
-        super(feedUrl, channel);
+    public OnetExtractor(String feedUrl, String channel, Map<String, String> dict) {
+        super(feedUrl, channel, dict);
     }
 
     @Override
