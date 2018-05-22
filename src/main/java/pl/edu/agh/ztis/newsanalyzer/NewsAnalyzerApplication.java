f40717f3d3c4e17ef4db7b62f1036c43440caa7d
Piotr Roksela <piotr@roksela.pl>
2018-05-22 11:30:57 +0200

    separation on polish and english dict

--- src/main/java/pl/edu/agh/ztis/newsanalyzer/NewsAnalyzerApplication.java
+++ src/main/java/pl/edu/agh/ztis/newsanalyzer/NewsAnalyzerApplication.java
@@ -17,8 +17,8 @@ public class NewsAnalyzerApplication implements CommandLineRunner {
     @Autowired
     private FeedRepository repository;
 
-    private String filepath;
-    private Map<String, String> dict;
+    private Map<String, String> plDict;
+    private Map<String, String> enDict;
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsAnalyzerApplication.class, args);
@@ -26,32 +26,32 @@ public class NewsAnalyzerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-	    filepath = "./src/main/resources/result_dict_v1-lowercase.csv";
         DictReader dictReader = new DictReader();
-        dict = dictReader.read(filepath);
+        plDict = dictReader.read("./src/main/resources/result_dict_v1_pl_extended.csv");
+        enDict = dictReader.read("./src/main/resources/result_dict_v1_en_extended.csv");
 
 
         List<Extractor> extractors = Arrays.asList(
-                new TVNExtractor("https://www.tvn24.pl/najwazniejsze.xml", "najważniejsze", dict),
-                new TVNExtractor("https://www.tvn24.pl/najnowsze.xml", "najnowsze", dict),
-                new TVNExtractor("https://www.tvn24.pl/wiadomosci-ze-swiata,2.xml", "świat", dict),
-                new TVNExtractor("https://www.tvn24.pl/biznes-gospodarka,6.xml", "biznes", dict),
-                new InteriaExtractor("http://fakty.interia.pl/feed", "fakty", dict),
-                new InteriaExtractor("http://fakty.interia.pl/prasa/feed", "prasa", dict),
-                new InteriaExtractor("http://fakty.interia.pl/swiat/feed", "świat", dict),
-                new InteriaExtractor("http://kanaly.rss.interia.pl/biznes.xml", "biznes", dict),
-                new WPExtractor("https://wiadomosci.wp.pl/rss.xml", "wiadomości", dict),
-                new OnetExtractor("https://wiadomosci.onet.pl/.feed", "wiadomości", dict),
-                new PAPExtractor("http://centrumprasowe.pap.pl/cp/pl/rss/13", "społeczne", dict),
-                new PAPExtractor("http://centrumprasowe.pap.pl/cp/pl/rss/1", "biznes", dict),
-                new PAPExtractor("http://centrumprasowe.pap.pl/cp/pl/rss/25", "wiadomości", dict),
-                new WSJExtractor("http://www.wsj.com/xml/rss/3_7085.xml", "world", dict),
-                new WSJExtractor("http://www.wsj.com/xml/rss/3_7014.xml", "business", dict),
-                new BBCExtractor("http://feeds.bbci.co.uk/news/rss.xml", "news", dict),
-                new BBCExtractor("http://feeds.bbci.co.uk/news/world/rss.xml", "world", dict),
-                new BBCExtractor("http://feeds.bbci.co.uk/news/business/rss.xml", "business", dict),
-                new BBCExtractor("http://feeds.bbci.co.uk/news/politics/rss.xml", "politics", dict),
-                new NYTExtractor("http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml", "news", dict)
+                new TVNExtractor("https://www.tvn24.pl/najwazniejsze.xml", "najważniejsze", plDict),
+                new TVNExtractor("https://www.tvn24.pl/najnowsze.xml", "najnowsze", plDict),
+                new TVNExtractor("https://www.tvn24.pl/wiadomosci-ze-swiata,2.xml", "świat", plDict),
+                new TVNExtractor("https://www.tvn24.pl/biznes-gospodarka,6.xml", "biznes", plDict),
+                new InteriaExtractor("http://fakty.interia.pl/feed", "fakty", plDict),
+                new InteriaExtractor("http://fakty.interia.pl/prasa/feed", "prasa", plDict),
+                new InteriaExtractor("http://fakty.interia.pl/swiat/feed", "świat", plDict),
+                new InteriaExtractor("http://kanaly.rss.interia.pl/biznes.xml", "biznes", plDict),
+                new WPExtractor("https://wiadomosci.wp.pl/rss.xml", "wiadomości", plDict),
+                new OnetExtractor("https://wiadomosci.onet.pl/.feed", "wiadomości", plDict),
+                new PAPExtractor("http://centrumprasowe.pap.pl/cp/pl/rss/13", "społeczne", plDict),
+                new PAPExtractor("http://centrumprasowe.pap.pl/cp/pl/rss/1", "biznes", plDict),
+                new PAPExtractor("http://centrumprasowe.pap.pl/cp/pl/rss/25", "wiadomości", plDict),
+                new WSJExtractor("http://www.wsj.com/xml/rss/3_7085.xml", "world", enDict),
+                new WSJExtractor("http://www.wsj.com/xml/rss/3_7014.xml", "business", enDict),
+                new BBCExtractor("http://feeds.bbci.co.uk/news/rss.xml", "news", enDict),
+                new BBCExtractor("http://feeds.bbci.co.uk/news/world/rss.xml", "world", enDict),
+                new BBCExtractor("http://feeds.bbci.co.uk/news/business/rss.xml", "business", enDict),
+                new BBCExtractor("http://feeds.bbci.co.uk/news/politics/rss.xml", "politics", enDict),
+                new NYTExtractor("http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml", "news", enDict)
         );
     
         int extractedCount;
